1f874b3f4ea0b884e4bb294584da9614e3a1639d
belivewhenisay <e227589@sbl.ch>
2022-12-13 20:14:51 +0100

    seitlich begrenzt

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const circle = document.querySelector("#circ")
  let dy = 0
+ let dx = 0
 
 
 function start() {
+
+dx = 1
+
     window.requestAnimationFrame(update)
 }
 
@@ -19,6 +23,20 @@ if (cy > 100){
 
     cy = cy + dy
     circle.setAttribute("cy", cy)
+
+
+
+    // seitwärts Bewegung
+    let cx = parseFloat (circle.getAttribute( "cx" ))
+
+    if (cx > 100){
+        dx = dx * (-1)
+    } else if (cx < 0) {
+        dx = dx * (-1)
+    }
+
+    cx = cx + dx
+    circle.setAttribute("cx", cx)
 
     window.requestAnimationFrame(update)
 }
